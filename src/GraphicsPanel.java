929940f6d53cf73f06759a91ccc7ad34235136c3
BT_3W2_02 <tommylo8247@Gmail.com>
2024-05-21 12:29:31 -0400

    finishing dashing method

--- src/GraphicsPanel.java
+++ src/GraphicsPanel.java
@@ -1,41 +1,89 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.IOException;
 import java.awt.image.*;
 
-public class GraphicsPanel extends JPanel {
+public class GraphicsPanel extends JPanel implements KeyListener, MouseListener{
     private BufferedImage baseKirbyImg;
     private BufferedImage starKirbyImg;
-    private double kirbyX;
-    private double kirbyY;
+    private BufferedImage background;
+    private Player player;
+    private boolean[] pressedKeys;
 
     public GraphicsPanel(){
-        kirbyX = 50;
-        kirbyY = 50;
-        try{
-            baseKirbyImg = ImageIO.read(new File("images/baseKirby.jpg"));
-            starKirbyImg = ImageIO.read(new File("images/starKirby.jpg"));
+       /* try{
+            background = ImageIO.read(new File("images/background.png"));
         }catch (IOException e){
             System.out.println(e.getMessage());
-        }
+        }*/
+        player = new Player("images/leftKirby.png", "images/rightKirby.jpg", "images/starLeft.png", "images/starRight.png");
+        pressedKeys = new boolean[128];
+        addKeyListener(this);
+        addMouseListener(this);
+        setFocusable(true);
+        requestFocusInWindow();
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        /*kirbyX += 1;
-        if(kirbyX + baseKirbyImg.getWidth() > 1000){
-            kirbyX = 0;
-        }
-        kirbyY += .5;
-        if(kirbyY + baseKirbyImg.getHeight() > 800){
-            kirbyY = 0;
-        }*/
+        //g.drawImage(background, 0, 0, null);
+        g.drawImage(player.getPlayerImg(), player.getxCoord(), player.getyCoord(), null);
 
-        g.drawImage(baseKirbyImg, (int)kirbyX, (int)kirbyY, null);
+        if(pressedKeys[65]){
+            player.faceLeft();
+            player.moveLeft();
+        }
+        if(pressedKeys[68]){
+            player.faceRight();
+            player.moveRight();
+        }
+        //up w
+        if(pressedKeys[87]){
+            player.moveUp();
+        }
+        //down s
+        if(pressedKeys[83]){
+            player.moveDown();
+        }
+        if(pressedKeys[39]){
+            player.faceRight();
+            player.moveRight();
+        }
+        if(pressedKeys[37]){
+            player.faceLeft();
+            player.moveLeft();
+        }
+        if(pressedKeys[40]){
+            player.moveDown();
+        }
+        if(pressedKeys[38]){
+            player.moveUp();
+        }
 
     }
 
+    public void keyTyped(KeyEvent e){}
+
+    public void keyPressed(KeyEvent e){
+        int key = e.getKeyCode();
+        pressedKeys[key] = true;
+    }
+    public void keyReleased(KeyEvent e){
+        int key = e.getKeyCode();
+        pressedKeys[key] = false;
+    }
+
+    public void mouseClicked(MouseEvent e){}
+    public void mousePressed(MouseEvent e){}
+    public void mouseReleased(MouseEvent e){
+    }
+    public void mouseEntered(MouseEvent e){}
+    public void mouseExited(MouseEvent e){}
 
 }
--- src/Player.java
+++ src/Player.java
@@ -3,22 +3,26 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 public class Player {
-    private double moveAmt;
+    private final double MOVE_AMT = 0.5;
     private BufferedImage right;
     private BufferedImage left;
-    private BufferedImage dash;
+    private BufferedImage dashingLeft;
+    private BufferedImage dashingRight;
     private boolean facingRight;
+    private boolean dashing;
     private double xCoord;
     private double yCoord;
 
-    public Player(String leftImg, String rightImg, String dashImg){
-        facingRight =true;
+    public Player(String leftImg, String rightImg, String dashLeft, String dashRight){
+        facingRight = true;
+        dashing = false;
         xCoord = 50;
         yCoord = 400;
         try{
             left = ImageIO.read(new File(leftImg));
             right = ImageIO.read(new File(rightImg));
-            dash = ImageIO.read(new File(dashImg));
+            dashingLeft = ImageIO.read(new File(dashLeft));
+            dashingRight = ImageIO.read(new File(dashRight));
         } catch (IOException e){
             System.out.println(e.getMessage());
         }
@@ -29,5 +33,70 @@ public class Player {
     }
     public int getyCoord(){
         return (int) yCoord;
+    }
+
+    public void faceRight(){
+        facingRight = true;
+    }
+
+    public void faceLeft(){
+        facingRight = false;
+    }
+
+    public void dashing(){
+        dashing = true;
+    }
+
+    public void moveRight(){
+        if(xCoord + MOVE_AMT + getPlayerImg().getWidth() <= 1000){
+            xCoord += MOVE_AMT;
+        }
+    }
+
+    public void moveLeft(){
+        if(xCoord - MOVE_AMT >= 0){
+            xCoord -= MOVE_AMT;
+        }
+    }
+
+    public void moveUp(){
+        if(yCoord - MOVE_AMT >= 0){
+            yCoord -= MOVE_AMT;
+        }
+    }
+    public void moveDown(){
+        if(yCoord + MOVE_AMT + getPlayerImg().getHeight() <= 800){
+            yCoord += MOVE_AMT;
+        }
+    }
+    public void dash(){
+        if(facingRight){
+            if(xCoord + 100 <= 1000){
+                xCoord += 100;
+            }
+        }else {
+            if (xCoord - 100 >= 0) {
+                xCoord -= 100;
+            }
+        }
+    }
+
+    public BufferedImage getPlayerImg(){
+        if(dashing && facingRight) {
+            return dashingRight;
+        }else if(dashing){
+            return dashingLeft;
+        }else if(facingRight){
+            return right;
+        }else{
+            return left;
+        }
+    }
+
+    public Rectangle getPlayerRect(){
+        int imageHeight = getPlayerImg().getHeight();
+        int imageWidth = getPlayerImg().getWidth();
+        Rectangle rect = new Rectangle((int)xCoord, (int)yCoord, imageWidth, imageHeight);
+        return rect;
     }
 }
